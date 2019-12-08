4fcb564b386d5cc245dd587a2376427098f6b4a8
skurik <standa.kurik@gmail.com>
2019-12-08 10:30:20 +0100

    Update the deploy script

---
+++ deploytools.ps1
@@ -0,0 +1,29 @@
+function Retry([Action] $action)
+{
+    $attempts = 3
+    $sleepInSeconds = 5
+    do
+    {
+        try
+        {
+            $action.Invoke();
+            break;
+        }
+        catch [Exception]
+        {
+            Write-Host $_.Exception.Message
+        }
+        $attempts--
+        if ($attempts -gt 0) { sleep $sleepInSeconds }
+    } while ($attempts -gt 0)
+}
+
+function MyFunction($inputArg)
+{
+    Throw $inputArg
+}
+
+function DoIt()
+{
+    Retry({MyFunction "Oh no! It happend again!"})
+}
