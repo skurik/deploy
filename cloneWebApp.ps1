0253832dcf66fa1fb5cbee92c8f7d7013ac2abae
skurik <standa.kurik@gmail.com>
2019-12-16 14:08:52 +0100

    WIP

--- cloneWebApp.ps1
+++ cloneWebApp.ps1
@@ -9,5 +9,12 @@ param (
 Write-Host "Fetching the source application info"
 $srcapp = Get-AzWebApp -ResourceGroupName $resourceGroup -Name $sourceAppName
 
+Write-Host $srcapp
+
+Write-Host "Resource group: $resourceGroup"
+Write-Host "Destination app name: $destinationAppName"
+Write-Host "Location: $location"
+Write-Host "AppServicePlan: $appServicePlan"
+
 Write-Host "Cloning the source application into $destinationAppName"
 $destapp = New-AzWebApp -ResourceGroupName $resourceGroup -Name $destinationAppName -Location $location -AppServicePlan $appServicePlan -SourceWebApp $srcapp
