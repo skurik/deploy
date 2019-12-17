4077e12c25e4f43e461c2b3faa460178634531d4
skurik <standa.kurik@gmail.com>
2019-12-17 08:52:29 +0100

    WIP

--- updateWebAppSettings.ps1
+++ updateWebAppSettings.ps1
@@ -2,9 +2,15 @@ param (
     [string] $resourceGroup,
     [string] $appName,
     [string] $connectionStringName,
-    [string] $databaseName
+    [string] $sqlServerName,
+    [string] $databaseName,
+    [string] $serverUserName,
+    [string] $serverUserPassword
 )
 
-$connstr = "Server=tcp:standaplayground.database.windows.net,1433;Initial Catalog=dynamicsqldb2;Persist Security Info=False;User ID=standaroot;Password=mypwd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+# develop:
+# Data Source=tcp:mews-develop-sql-weu.database.windows.net,1433;Initial Catalog=mews-develop-db;User ID=Mews-D79D02B7-2C1D-403D-8648-ABD3629D30EB@mews-develop-sql-weu;Password=4E0366BD-CCA2-49D0-9B4C-677A0DA9029A
+
+$connstr = "Data Source=tcp:$sqlServerName.database.windows.net,1433;Initial Catalog=$databaseName;User ID=$serverUserName@$sqlServerName;Password=$serverUserPassword"
 
 Set-AzWebApp -ResourceGroupName $resourceGroup -Name $appName -ConnectionStrings @{ $connectionStringName = @{ Type = "SQLAzure"; Value = $connstr }
