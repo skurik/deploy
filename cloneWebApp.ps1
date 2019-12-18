4f6d3a90406948cf87ff1bdd46b150b33a35c6d0
skurik <standa.kurik@gmail.com>
2019-12-18 09:01:52 +0100

    WIP

--- cloneWebApp.ps1
+++ cloneWebApp.ps1
@@ -10,4 +10,4 @@ Write-Host "Fetching the source application info"
 $srcapp = Get-AzWebApp -ResourceGroupName $resourceGroup -Name $sourceAppName
 
 Write-Host "Cloning the source application into $destinationAppName"
-$destapp = New-AzWebApp -ResourceGroupName $resourceGroup -Name $destinationAppName -Location $location -AppServicePlan $appServicePlan -SourceWebApp $srcapp
+$destapp = New-AzWebApp -ResourceGroupName $resourceGroup -Name $destinationAppName -Location $location -AppServicePlan $appServicePlan -SourceWebApp $srcapp -IncludeSourceWebAppSlots
