a69c36ad900c58491627c885c0056a38366384c9
skurik <standa.kurik@gmail.com>
2019-12-18 10:37:21 +0100

    WIP

--- createSqlDatabase.ps1
+++ createSqlDatabase.ps1
@@ -8,6 +8,8 @@ param (
     [string] $databaseName
 )
 
+# Creating a new SQL server is very slow
+
 try
 {
 	Remove-AzSqlDatabase -ResourceGroupName $resourceGroup -ServerName $serverName -DatabaseName $databaseName -Force	
@@ -18,21 +20,21 @@ catch
   	Write-Host $_
 }
 
-try
-{
-	Remove-AzSqlServer -ResourceGroupName $resourceGroup -ServerName $serverName -Force
-}
-catch
-{
-  	Write-Host "Could not remove the existing SQL server"
-  	Write-Host $_
-}
+#try
+#{
+#	Remove-AzSqlServer -ResourceGroupName $resourceGroup -ServerName $serverName -Force
+#}
+#catch
+#{
+#  	Write-Host "Could not remove the existing SQL server"
+#  	Write-Host $_
+#}
 
-$securePassword = ConvertTo-SecureString -String $serverUserPassword -AsPlainText -Force
-$serverCredentials = New-Object System.Management.Automation.PSCredential ($serverUserName, $securePassword)
+# $securePassword = ConvertTo-SecureString -String $serverUserPassword -AsPlainText -Force
+# $serverCredentials = New-Object System.Management.Automation.PSCredential ($serverUserName, $securePassword)
 
-Write-Host "Creating a new SQL server ($serverName)"
-New-AzSqlServer -ResourceGroupName $resourceGroup -Location $location -ServerName $serverName -ServerVersion "12.0" -SqlAdministratorCredentials $serverCredentials
+# Write-Host "Creating a new SQL server ($serverName)"
+# New-AzSqlServer -ResourceGroupName $resourceGroup -Location $location -ServerName $serverName -ServerVersion "12.0" -SqlAdministratorCredentials $serverCredentials
 
 Write-Host "Creating a new SQL database ($databaseName) on $serverName"
 New-AzSqlDatabase -ResourceGroupName $resourceGroup -ServerName $serverName -Edition $edition -DatabaseName $databaseName
