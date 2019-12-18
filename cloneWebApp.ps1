aac885e9fe1a4ca85e408df0c60ec797562d59cf
skurik <standa.kurik@gmail.com>
2019-12-18 10:02:34 +0100

    WIP

--- cloneWebApp.ps1
+++ cloneWebApp.ps1
@@ -8,8 +8,12 @@ param (
 
 Remove-AzWebApp -ResourceGroupName $resourceGroup -Name $destinationAppName -Force
 
-Write-Host "Fetching the source application info"
-$srcapp = Get-AzWebApp -ResourceGroupName $resourceGroup -Name $sourceAppName
+# There is a bug in Azure - while the application settings are cloned, the "Deployment slot settings" flag is not.
+# This breaks our swap procedure as the staging slot will not start up due to its WEBSITE_SITE_TYPE variable being "staging"
+#
+# Write-Host "Fetching the source application info"
+# $srcapp = Get-AzWebApp -ResourceGroupName $resourceGroup -Name $sourceAppName
 
 Write-Host "Cloning the source application into $destinationAppName"
-$destapp = New-AzWebApp -ResourceGroupName $resourceGroup -Name $destinationAppName -Location $location -AppServicePlan $appServicePlan -SourceWebApp $srcapp -IncludeSourceWebAppSlots
+$destapp = New-AzWebApp -ResourceGroupName $resourceGroup -Name $destinationAppName -Location $location -AppServicePlan $appServicePlan
+# $destapp = New-AzWebApp -ResourceGroupName $resourceGroup -Name $destinationAppName -Location $location -AppServicePlan $appServicePlan -SourceWebApp $srcapp -IncludeSourceWebAppSlots
