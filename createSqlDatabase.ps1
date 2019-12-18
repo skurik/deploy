13c6795623b59863a0d6523b610e214409b694f9
skurik <standa.kurik@gmail.com>
2019-12-18 07:08:19 +0100

    WIP

--- createSqlDatabase.ps1
+++ createSqlDatabase.ps1
@@ -8,7 +8,6 @@ param (
     [string] $databaseName
 )
 
-
 $securePassword = ConvertTo-SecureString -String $serverUserPassword -AsPlainText -Force
 $serverCredentials = New-Object System.Management.Automation.PSCredential ($serverUserName, $securePassword)
 
